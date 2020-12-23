13e0c66288ea2f28f34bbabdbae01b44f7d2390e
Biaolg <longbiaokuaile@qq.com>
2020-12-23 23:51:56 +0800

    todo-层叠问题

--- LesiaGuiEngine.js
+++ LesiaGuiEngine.js
@@ -9,49 +9,14 @@ class LesiaGuiEngine {
     this.defaultStyle = {};
     this.Rendering(); //首次渲染
   }
-  getDynamic() {
-    let slef = this;
-    let element = JSON.parse(JSON.stringify(slef.data.element));
-    let rongq = {};
-
-    Object.keys(element).forEach((key) => {
-      if (key != "defaultStyle") {
-        let obj = element[key];
-        Object.keys(obj).forEach((item) => {
-          rongq[key] = new Proxy(obj[item], {
-            get: function (target, propKey, receiver) {
-              return Reflect.get(target, propKey, receiver);
-            },
-            set: function (target, propKey, value, receiver) {
-              baogao([key, item, propKey], value);
-              console.log(propKey);
-              return Reflect.set(target, propKey, value, receiver);
-            },
-          });
-        });
-      }
-    });
-
-    function baogao(arrkey, value) {
-      let [key1, key2, key3] = arrkey;
-      console.log(arrkey);
-      slef.data.element[key1][key2][key3] = value;
-      slef.Rendering();
-    }
-
-    return element;
-  }
-  getStatic() {
-    return this.data.element;
-  }
   Animation(fn) {
     let b = fn(this.data.element);
     if (!b) {
-      setTimeout(() => {
+      requestAnimationFrame(() => {
         this.ctx.clearRect(0, 0, this.data.size.w, this.data.size.h);
         this.Animation(fn);
         this.Rendering();
-      }, 1000 / 60);
+      });
     }
   }
   Rendering(obj = this.data.element) {
@@ -89,8 +54,49 @@ class LesiaGuiEngine {
       this.ctx[type]();
     });
   }
-  renderImg(obj){
-
+  renderImg(obj) {//绘制图片
+    let { style, x, y, w, h, sx, sy, swidth, sheight, dom } = obj;
+    this.renderStyle(style, () => {
+      if (sx != undefined) {
+        this.ctx.drawImage(dom, sx, sy, swidth, sheight, x, y, w, h);
+      } else if (w != undefined) {
+        this.ctx.drawImage(dom, x, y, w, h);
+      } else {
+        this.ctx.drawImage(dom, x, y);
+      }
+    });
+    return { x, y, w, h }//todo-碰撞检测
   }
-  render
+  renderImageData(obj) {//像素操作
+    let { style, data, w, h, x, y } = obj;
+    this.renderStyle(style, () => {
+      let imgData = this.ctx.createImageData(w, h);
+      imgData.data = data;
+      this.ctx.putImageData(imgData, x, y);
+    })
+  }
+  renderRoute(obj) {//路径
+    let { style, routeArr, type } = obj;
+    this.renderStyle(style, () => {
+      this.ctx.moveTo(routeArr[0][0], routeArr[0][1]);
+      let Len = routeArr.length;
+      for (let i = 1; i < Len; i++) {
+        this.ctx.lineTo(routeArr[i][0], routeArr[i][1]);
+      }
+      if (type == "fill") {
+        this.ctx.closePath();
+      }
+      this.ctx[type]();
+    })
+  }
+  renderArc(obj) {
+    let { style, x, y, r, s, e, c, type } = obj;
+    if (c == undefined) {
+      c = false;
+    }
+    this.renderStyle(style, () => {
+      this.ctx.arc(x, y, r, s * Math.PI, e * Math.PI, c);
+      this.ctx[type]();
+    })
+  }
 }
--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 window.onload = function () {
-  this.$ = (id) => document.querySelector(id);
-  let box = $(".box");
+    this.$ = (id) => document.querySelector(id);
+    let box = $(".box");
 
-  let canObj = {
-    type: "2d",
-    size: {
-      w: 1920,
-      h: 1080,
-    },
-    target: box,
-    element: {
-      renderText: {
-        helloword: {
-          style: {
-            fillStyle: "pink",
-            font: "192px Arial",
-          },
-          content: "Hello world!",
-          x: 100,
-          y: 100,
-          type: "fillText", //fillText 填充 strokeText 描边
+    let canObj = {
+        type: "2d",
+        size: {
+            w: 1920,
+            h: 1080,
         },
-      },
-    },
-  };
+        target: box,
+        element: {
+            renderArc: {
+            },
+            renderText: {
+                helloword: {
+                    style: {
+                        fillStyle: "pink",
+                        strokeStyle: "#aaf5f5",
+                        font: "192px Arial",
+                        textAlign: "center",
+                        lineWidth: 10
+                    },
+                    content: "Hello world!",
+                    x: 960,
+                    y: 600,
+                    type: "strokeText", //fillText 填充 strokeText 描边
+                },
+            }
+        },
+    };
 
-  let lesiaUi = new LesiaGuiEngine(canObj);
+    let lesiaUi = new LesiaGuiEngine(canObj);
 
-  lesiaUi.Animation(function (element) {
-    let helloword = element.renderText.helloword;
-    helloword.x += 1;
-    helloword.y += 2;
-  });
+    lesiaUi.Animation(function (element) {
+        let helloword = element.renderText.helloword;
+        helloword.x += Math.random() - 0.5;
+        helloword.y += Math.random() - 0.5;
+    });
 };
